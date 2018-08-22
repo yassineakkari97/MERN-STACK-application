d7e7e1ca5d449bd8c4809716fae7d686dd80dde0
yassine akkari <joudakkari2015@gmail.com>
2018-08-22 23:18:45 +0100

    first commit

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,46 @@
+import uuid from "uuid";
+
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING
+} from "../actions/types";
+
+const initialState = {
+  items: [],
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ITEMS:
+      console.log("itemreducer :" + action.payload);
+      return {
+        ...state,
+        items: action.payload,
+        loading: false
+      };
+    case DELETE_ITEM:
+      console.log("item redex");
+
+      return {
+        ...state,
+        items: state.items.filter(item => item._id !== action.payload)
+      };
+    //step2
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      };
+
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+}
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://becha97:bechaakkari97@ds125482.mlab.com:25482/mern-shopping1"
+};
